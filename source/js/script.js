bea4033eb72fd2f03c49bc3a9ffc7fc240c5866f
Andy <adrianrazor@ukr.net>
2021-10-31 10:02:13 +0200

    created-header
    Created header, navigation, modal window (mobile version), hover effects

---
+++ source/js/script.js
@@ -0,0 +1,51 @@
+// Бургер меню
+var burger = document.querySelector('.header__burger')
+var nav = document.querySelector('.nav')
+
+// Анимация бургер иконки
+var closeNav = document.querySelector('.header__burger-line')
+
+if (burger) {
+    burger.addEventListener (
+        'click',
+        function () {
+            nav.classList.toggle('nav--shown')
+            closeNav.classList.toggle('header__burger-x-icon')
+        }
+    )
+}
+
+// Ссылки в бургер меню
+// var link = document.querySelectorAll('nav__link')
+
+// if (link) {
+//     link.addEventListener (
+//         'click',
+//         function () {
+//             nav.classList.add('nav--shown')
+//         }
+//     )
+// }
+
+// Ссылки в бургер меню
+// var link = document.querySelectorAll('nav__link')
+
+// link.forEach(function(close) {
+//     close.addEventListener ('click', closeClick)
+// })
+
+// function closeClick () {
+//     nav.classList.add('nav--shown')
+// }
+
+
+// Спойлер в секции "Основные специализации"
+var spoiler = document.querySelectorAll('.spoiler__name-block')
+
+spoiler.forEach(function(item) {
+    item.addEventListener ('click', itemClick)
+})
+
+function itemClick () {
+    this.nextElementSibling.classList.toggle('spoiler--active')
+}
